f0114cfe7146f99fa64518663c04cc604a45db00
Gonzales <k2gonzales@UCSD.EDU>
2020-01-28 13:13:27 -0800

    changes to dosing.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 To Run the script:
 Place the contect of these zip files in a directory
 and run from terminal : python dosing.py for the chart
-python test_dosing.py for the csv
-
-Note I am using Pytrhon v 2.7
+python test_dosing.py w02 Y 280 result.csv 
+(3 parameters )
+Note I am using Python v 2.7
--- test_dosing.py
+++ test_dosing.py
@@ -1,9 +1,11 @@
-import pandas as pd
 
-data = pd.read_csv('t2_registry 20190619.csv').query('VISCODE == "w02" and SVDOSE == "Y"')
-data2 = pd.read_csv('t2_ec 20190619.csv').query('ECSDSTXT != 280')
+import pandas as pd
+import sys
+
+data = pd.read_csv('t2_registry 20190619.csv').query('VISCODE == "%s" and SVDOSE == "%s"'% (sys.argv[1],sys.argv[2]))
+data2 = pd.read_csv('t2_ec 20190619.csv').query('ECSDSTXT != %i' % (int(sys.argv[3])))
 df = pd.DataFrame(data,columns=['ID','RID','VISCODE','SVDOSE','USERID'])
 df2 =pd.DataFrame(data2,columns=['ID','RID','VISCODE','ECSDSTXT','USERID'])
 keyin = ['RID','VISCODE']
 df3 = pd.merge(df,df2, on=keyin, how='inner')
-df3.to_csv('result.csv')
+df3.to_csv('%s' % (sys.argv[4]))
