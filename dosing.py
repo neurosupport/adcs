c9c96418e2a523f234a2960b1e6d496790d8349d
Kenneth Gonzales <k2gonzales@ucsd.edu>
2020-01-29 07:37:50 -0800

    Changes to dosing.py

--- dosing.py
+++ dosing.py
@@ -1,13 +1,26 @@
 import pandas as pd
-import plotly.express as px
+
 import plotly.graph_objects as go
+
 data = pd.read_csv('t2_registry 20190619.csv').query('VISCODE != "bl" and SVPERF == "Y"')
 data['Total'] = 1
 df = pd.DataFrame(data)
-ds = pd.DataFrame(data).groupby('VISCODE').count()
 
-fig = px.pie(df, values='Total',names ='VISCODE',
-             title='VISCODE from Registry', labels={'ID':'Total'}
-            )
-fig.update_traces(textposition='inside', textinfo='percent+label')
-fig.show()
+fig = go.Figure(go.Pie(
+    name='',
+    values=df['Total'],
+    labels=df['VISCODE'],
+    text=df['VISCODE'],
+
+    hovertemplate="VISCODE: %{label} <br>Percentage: %{percent}  "
+))
+fig.update_layout(
+    title='VISCODE from Registry',
+
+    font=dict(
+
+        size=18,
+        color="#7f7f7f"
+
+    )
+)
